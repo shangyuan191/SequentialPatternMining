c48c83cc2e4f8c3d4d9ed6a9e009320947ed6077
shangyuan191 <p76124752@gs.ncku.edu.tw>
2024-03-17 23:48:58 +0800

    20240317_2346 convert TDB into a dict,key is frozenset of TID(contains 1 TID),and values is a frozenset of frozenset(all items in same transaction will be combine into one frozenset,in addition,in one transaction,maybe more than one item will be buy simultaneously,those items will be combine into a sub-frozenset)

--- SPM.py
+++ SPM.py
@@ -3,15 +3,23 @@ from itertools import combinations
 import csv
 def readfile(file_name):
     try:
-        transactions=[]
+        transactions={}
         with open(file_name,'r') as file:
             for line in file:
                 line_list=line.strip().split()
                 print(line_list)
-                # for i in range(len(line_list)):
-                #     line_list[i]=int(line_list[i])
-                # sorted(line_list)
-                # transactions.append(line_list)
+                dic={}
+                for i in range(1,len(line_list),2):
+                    time_stamp=int(line_list[i])
+                    item=int(line_list[i+1])
+                    if time_stamp not in dic:
+                        dic[time_stamp]=set([item])
+                    else:
+                        dic[time_stamp].add(item)
+                for key in dic:
+                    dic[key]=frozenset(dic[key])
+                TID=int(line_list[0])
+                transactions[frozenset([TID])]=frozenset(list(dic.values()))
             return transactions
     except FileNotFoundError:
         print(f"File {file_name} not found.")
@@ -29,6 +37,10 @@ def main():
     print("min_sup:", min_sup)
     print("file_name:", file_name)
     transactions=readfile(file_name)
+    # print(transactions)
+    # for transaction in transactions:
+    #     print(transaction)
+    #     print(transactions[transaction])
 
 if __name__ == "__main__":
     main()
