3328d7d5ad2f7701e7be9bd832a7129dcae59aa0
shangyuan191 <p76124752@gs.ncku.edu.tw>
2024-03-19 17:41:48 +0800

    20240319_1741 prune TDB by those one_item which not satisfy min_sup

--- SPM.py
+++ SPM.py
@@ -3,24 +3,31 @@ from itertools import combinations
 import csv
 def readfile(file_name):
     try:
-        transactions={}
+        TDB={}
+        one_item={}
+        # count=0
         with open(file_name,'r') as file:
             for line in file:
                 line_list=line.strip().split()
-                print(line_list)
+                # print(line_list)
                 dic={}
                 for i in range(1,len(line_list),2):
-                    time_stamp=int(line_list[i])
+                    TID=int(line_list[i])
                     item=int(line_list[i+1])
-                    if time_stamp not in dic:
-                        dic[time_stamp]=set([item])
+                    if item not in one_item:
+                        one_item[item]=0
+                    if TID not in dic:
+                        dic[TID]=set([item])
                     else:
-                        dic[time_stamp].add(item)
+                        dic[TID].add(item)
                 for key in dic:
                     dic[key]=frozenset(dic[key])
-                TID=int(line_list[0])
-                transactions[frozenset([TID])]=frozenset(list(dic.values()))
-            return transactions
+                SID=int(line_list[0])
+                TDB[frozenset([SID])]=frozenset(list(dic.values()))
+                # count+=1
+                # if count>=10:
+                #     break
+            return TDB,one_item
     except FileNotFoundError:
         print(f"File {file_name} not found.")
     except Exception as e:
@@ -36,11 +43,46 @@ def main():
 
     print("min_sup:", min_sup)
     print("file_name:", file_name)
-    transactions=readfile(file_name)
-    # print(transactions)
-    # for transaction in transactions:
-    #     print(transaction)
-    #     print(transactions[transaction])
+    TDB,one_item=readfile(file_name)
+    for SID in TDB:
+        tmp_set=set()
+        transactions=TDB[SID]
+        for transaction in transactions:
+            for item in transaction:
+                tmp_set.add(item)
+        for item in tmp_set:
+            one_item[item]+=1
+    cnt=0
+    for item in one_item:
+        if one_item[item]<min_sup:
+            cnt+=1
+            one_item[item]=-1
+    print(len(TDB))
+    print(len(one_item)-cnt)
+
+    TDB_after_one_item_prune={}
+    for SID in TDB:
+        transactions=TDB[SID]
+        tmp_set=set()
+        for transaction in transactions:
+            tmp_tmp_set=set()
+            for item in transaction:
+                if one_item[item]!=-1:
+                    tmp_tmp_set.add(item)
+            if len(tmp_tmp_set)>0:
+                tmp_set.add(frozenset(tmp_tmp_set))
+        if len(tmp_set)>0:
+            TDB_after_one_item_prune[SID]=tmp_set
+
+
+ 
+    # for SID in TDB_after_one_item_prune:
+    #     transactions=TDB_after_one_item_prune[SID]
+    #     print(f"SID = {SID}")
+    #     print(f"transactions = {transactions}")
+    print(f"Before pruning: len is {len(TDB)}")
+    print(f"After pruning: len is {len(TDB_after_one_item_prune)}")
+
 
 if __name__ == "__main__":
     main()
